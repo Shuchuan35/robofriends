50391ba04d0a108c87d9ef9162b92ecb173b031f
scream <hou.jennifer35@gmail.com>
2019-05-07 18:53:35 -0400

    Updates

---
+++ precache-manifest.e1cd1c81512f43f00a303371e46f0b56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8196e5e422f4c83b2357aad498b409a5",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "b77e2456ce5e33766990",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "1eabd4d050ffc45baa76",
+    "url": "/robofriends/static/css/main.c521ae50.chunk.css"
+  },
+  {
+    "revision": "b77e2456ce5e33766990",
+    "url": "/robofriends/static/js/2.b4b6e5cc.chunk.js"
+  },
+  {
+    "revision": "1eabd4d050ffc45baa76",
+    "url": "/robofriends/static/js/main.1659672c.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
